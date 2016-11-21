fd96780fcf1734bc7fb91a41a39bb96f3d1ce85a
jaumealonso21 <jaumealonso@hotmail.com>
2016-11-21 20:07:08 +0100

    version 0.0

---
+++ public_html/cronometre.js
@@ -0,0 +1,20 @@
+
+var data;
+
+function iniciar() {
+    data = new Date();
+    console.log(data.getMilliseconds());
+}
+
+function crono() {
+    
+}
+
+function inicia() {
+    data = new Date();
+    
+}
+
+function pausa() {
+    
+}
